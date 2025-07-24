b5d4c698b516287c94ba5072ceab1d744e4ea216
yfnel <221569045+yfnel@users.noreply.github.com>
2025-07-24 17:17:34 +0200

    Create svango.yml

--- svango/config/dynaconf_hooks.py
+++ svango/config/dynaconf_hooks.py
@@ -1,8 +1,14 @@
-from django_auth_ldap import config
+try:  # pragma: no cover
+    from django_auth_ldap import config
+except ImportError:  # pragma: no cover
+    config = None
+
+
 from dynaconf.hooking import HookableSettings
 
 
 def post(settings: HookableSettings) -> dict:
     data = {'dynaconf_merge': True}
-    data['AUTH_LDAP_USER_SEARCH'] = config.LDAPSearch(**settings.AUTH_LDAP_USER_SEARCH_PARAMS)
+    if config:  # pragma: no cover
+        data['AUTH_LDAP_USER_SEARCH'] = config.LDAPSearch(**settings.AUTH_LDAP_USER_SEARCH_PARAMS)
     return data
